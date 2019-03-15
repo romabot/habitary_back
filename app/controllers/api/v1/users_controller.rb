10e38bdbc9563d17b17c6722f66a1908f6f1125d
Roman Pirag <romanpirag@gmail.com>
2019-03-15 16:44:54 -0400

    Auth server side finished

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-#    skip_before_action :authorized, only: [:create]
+   skip_before_action :authorized, only: [:create]
     
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
     def index
         @users = User.all
         render json: @users
@@ -12,12 +17,13 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-        render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-        render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
     end
 
     private
